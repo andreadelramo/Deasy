f731934d399559fce6e55caac9ce146e5fc63f22
andreadelramo <167461492+andreadelramo@users.noreply.github.com>
2026-04-26 22:03:01 +0200

    Replace Figma API handler with a simple response

--- api/index.js
+++ api/index.js
@@ -1,13 +1,3 @@
-export default async function handler(req, res) {
-  const fileKey = req.query.fileKey;
-
-  const response = await fetch(`https://api.figma.com/v1/files/${fileKey}`, {
-    headers: {
-      "X-Figma-Token": process.env.FIGMA_TOKEN
-    }
-  });
-
-  const data = await response.json();
-
-  res.status(200).json(data);
+export default function handler(req, res) {
+  res.status(200).json({ ok: true });
 }
