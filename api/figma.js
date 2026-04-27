9397745b92418d41e61705e1c360aef8710eb9f3
andredelramo <andreadelramo@gmail.com>
2026-04-27 09:48:50 +0200

    Update figma.js

--- api/figma.js
+++ api/figma.js
@@ -1,10 +1,9 @@
 export default async function handler(req, res) {
-  const fileKey = req.query.fileKey;
+  const fileKey = "DdrBhynQU7O9ma5Rr8vzFj";
 
   const response = await fetch(`https://api.figma.com/v1/files/${fileKey}`, {
-    method: "GET",
     headers: {
-      "X-Figma-Token": `${process.env.FIGMA_TOKEN}`
+      "X-Figma-Token": process.env.FIGMA_TOKEN
     }
   });
 
